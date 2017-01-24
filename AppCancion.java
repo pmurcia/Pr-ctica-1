c9c797a9b7fef93998e24136588408418dec9c7e
pmurcia <p.murcia.morilla@gmail.com>
2017-01-24 20:00:46 +0100

    Used stream to filter, and tried new ways to code

--- AppCancion.java
+++ AppCancion.java
@@ -4,49 +4,65 @@ public class AppCancion
 {
 	public static void main(String[] args)
 	{
-		// ¿Por qué no puede ir fuera del main?
+		// Creamos un array dinámico de canciones
 		ArrayList<Cancion> canciones = new ArrayList<Cancion>();
-		canciones.add(new Cancion(".mp3","Call - Francisco Yates",256,5));
-		canciones.add(new Cancion(".ogg","Dragostea Din Tei - OZone",190,2));
-		canciones.add(new Cancion(".wav","Gangsta's Paradise - Coolio ft. L.V",192,4));
+		
+		// Añadimos tres canciones
+		canciones.add(new Cancion("Call - Francisco Yates",256,5));
+		canciones.add(new Cancion("Dragostea Din Tei - OZone",190,2));
+		canciones.add(new Cancion("Gangsta's Paradise - Coolio ft. L.V",192,4));
 
+		// Mostramos toda su información por pantalla (a lo Ruby)
+		System.out.println("Información de las canciones:");
 		canciones.forEach(cancion -> {
-			System.out.println(cancion.getNombre());
-			System.out.println(cancion.getFormato());
-			System.out.println(cancion.getCalidad() + " kbps");
-			System.out.println(cancion.getValoracion() + "/5");
+			System.out.println("Nombre: " + cancion.getNombre());
+			System.out.println("Formato: " + Cancion.getFORMATO());
+			System.out.println("Calidad: " + cancion.getCalidad() + " kbps");
+			System.out.println("Valoración: " + cancion.getValoracion() + "/5");
 			System.out.println("");
 		});
 
-		System.out.println(canciones.get(0).getFormato());
-		System.out.println(canciones.get(1).getNombre());
+		// Mostramos por pantalla alguna de su información
+		System.out.println("Formato de la primera cancion: " + Cancion.getFORMATO());
+		System.out.println("Nombre de la segunda cancion: " + canciones.get(1).getNombre());
 		canciones.get(2).setValoracion(5);
 		System.out.println("");
 
-		canciones.forEach(cancion -> {
-			System.out.println(cancion.getInfo());
-		});
+		// Mostramos la información de la canción de forma ordenada
+		System.out.println("Información de las canciones en forma de una sola línea:");
+		canciones
+			.stream()
+			.map(Cancion::getInfo)
+			.forEach(System.out::println);
 		System.out.println("");
 
-		canciones.forEach(cancion -> {
-			System.out.println(cancion.getFormato());
-		});
-		System.out.println("");
-
-		canciones.forEach(cancion -> {
-			if(cancion.getValoracion() > 3)
-			{
-				System.out.println(cancion.getInfo());
-			}
-		});
-		System.out.println("");
+		// Obtenemos el formato de las canciones
+		System.out.println("Formato de las canciones:");
+		canciones
+			.stream()
+			.map(cancion -> Cancion.getFORMATO())
+			/* En el caso que no tuviesen el mismo formato, se debería escribir
+			Cancion::getFormato, modificando la clase */
+			.forEach(System.out::println);
 		
-		canciones.forEach(cancion -> {
-			if(cancion.getCalidad() == 256)
-			{
-				System.out.println(cancion.getInfo());
-			}
-		});
 		System.out.println("");
+
+		// Obtenemos las canciones con una valoración mayor que 3
+		System.out.println("Canciones con valoración mayor que 3:");
+		canciones
+			.stream()
+			.filter(cancion -> cancion.getValoracion() > 3)
+			.map(Cancion::getInfo)
+			.forEach(System.out::println);
+						
+		System.out.println("");
+
+		// Obtenemos las canciones con una calidad de 256 kbps		
+		System.out.println("Canciones con calidad de 256 kbps:");
+		canciones
+			.stream()
+			.filter(cancion -> cancion.getCalidad() == 256)
+			.map(Cancion::getInfo)
+			.forEach(System.out::println);
 	}
 }
--- Cancion.java
+++ Cancion.java
@@ -2,47 +2,62 @@ import java.util.*;
 
 public class Cancion
 {
-	String formato;
-	String nombre;
-	int calidad = 128;
-	int valoracion;
+	// Atributos de clase
+	public static String FORMATO = "mp3";
 
-	public Cancion(String formato, String nombre, int calidad, int valoracion)
+	// Método que devuelve el formato de las canciones
+	public static String getFORMATO()
 	{
-		this.formato = formato;
+		return FORMATO;
+	}
+
+	// Método que modifica el formato de las canciones
+	public static void setFORMATO(String NUEVO_FORMATO)
+	{
+		FORMATO = NUEVO_FORMATO;
+	}
+	
+	// Atributos de instancia
+	public String nombre;
+	public int calidad;
+	public int valoracion;
+
+	// Constructor
+	public Cancion(String nombre, int calidad, int valoracion)
+	{
 		this.setNombre(nombre);
 		this.setCalidad(calidad);
 		this.setValoracion(valoracion);
 	}
-
-	public String getFormato()
-	{
-		return formato;
-	}
 	
+	// Método que devuelve el nombre
 	public String getNombre()
 	{
 		return nombre;
 	}
 	
+	// Método que devuelve la calidad
 	public int getCalidad()
 	{
 		return calidad;
 	}
 	
+	// Método que devuelve la valoración
 	public int getValoracion()
 	{
 		return valoracion;
 	}
 	
+	// Método que modifica el nombre de la canción
 	public void setNombre(String nombre)
 	{
 		this.nombre = nombre;
 	}
 
+	// Método que modifica la calidad de la canción
 	public void setCalidad(int calidad)
 	{
-		if(calidad == 128 || calidad == 192 || calidad == 256)
+		if(calidad == 192 || calidad == 256)
 		{
 			this.calidad = calidad;
 		}
@@ -52,6 +67,7 @@ public class Cancion
 		}
 	}
 	
+	// Método que modifica la valoración de la canción
 	public void setValoracion(int valoracion)
 	{
 		if(valoracion < 0)
@@ -68,9 +84,10 @@ public class Cancion
 		}
 	}
 
+	// Método que devuelve la información de la canción en un String
 	public String getInfo()
 	{
-		return this.nombre + this.formato + ", "
-		 + this.calidad + " kbps, " + this.valoracion + "/5";
+		return nombre + "." + FORMATO + ", "
+		 + calidad + " kbps, " + valoracion + "/5";
 	}
 }
